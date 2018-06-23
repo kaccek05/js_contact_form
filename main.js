4bbc4aaa69d1f1f08f31bc6f46d0dfb5dd80d06d
Kacper Czuk <kaccek05.99@gmail.com>
2018-06-23 15:27:49 +0200

    Add showing errors

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 console.log("Loading complete")
 $(document).ready(function() {
-  $("#button").click(function() {
-  	console.log("Button clicked")
+  $("#button").click(function(button) {
+    $.post("http://csbackend.s1.themasters.io/api/v1/contact_messages")
+    .done(function() {
+      alert("second success");
+    })
+    .fail(function(data) {
+      var errors = data.responseJSON.errors
+      $("#email-error-message").text(errors.email[0])
+      $("#type-error-message").text(errors.type[0])
+      $("#message-error-message").text(errors.content[0])
+    })
   })
 })
