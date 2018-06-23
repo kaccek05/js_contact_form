3f674eb18a0295495d8b5dd40ed41fda748f0989
Kacper Czuk <kaccek05.99@gmail.com>
2018-06-23 15:57:10 +0200

    Add taking email, type and message

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 console.log("Loading complete")
 $(document).ready(function() {
   $("#button").click(function(button) {
-    $.post("http://csbackend.s1.themasters.io/api/v1/contact_messages")
-    .done(function() {
-      alert("second success");
+    $("#email-error-message").text('')
+    $("#type-error-message").text('')
+    $("#message-error-message").text('')
+
+    var data = {
+      email: $("#email").val(),
+      type: $("#type").val(),
+      content: $("#message").val(),
+    }
+    $.post("http://csbackend.s1.themasters.io/api/v1/contact_messages", data)
+    .done(function(data) {
+      console.log(data)
     })
     .fail(function(data) {
       var errors = data.responseJSON.errors
-      $("#email-error-message").text(errors.email[0])
-      $("#type-error-message").text(errors.type[0])
-      $("#message-error-message").text(errors.content[0])
+      if (errors.email) {
+        $("#email-error-message").text(errors.email[0])
+      }
+      if (errors.type) {
+        $("#type-error-message").text(errors.type[0])
+      }
+      if (errors.content) {
+        $("#message-error-message").text(errors.content[0])
+      }
     })
   })
 })
